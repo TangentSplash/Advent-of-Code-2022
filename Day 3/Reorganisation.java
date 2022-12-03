415085733aa94738dba570233fe6599be42da81c
TangentSplash <68692742+TangentSplash@users.noreply.github.com>
2022-12-03 17:10:47 +0000

    Day 3 Part 2
    Day 3 Part 2 Completed

--- Day 3/Reorganisation.java
+++ Day 3/Reorganisation.java
@@ -1,5 +1,5 @@
 /* Advent of Code 2022
- * Day 3 Part 1
+ * Day 3 Part 2
  */
 
 import java.io.File;
@@ -12,16 +12,29 @@ public class Reorganisation {
         Scanner input = new Scanner(file);
 
         String backpackItems;
-        int totalPriority=0;
+        int totalSinglePriority=0;
+        int totalGroupPriority=0;
+        int i=0;
+        Rucksack Group[]=new Rucksack[3];
         while (input.hasNextLine())
         {
             backpackItems=input.nextLine();
             Rucksack rucksack = new Rucksack(backpackItems);
-            totalPriority+=rucksack.getPriority();
+            rucksack.findSingleBackpackOverlap();
+            totalSinglePriority+=rucksack.getPriority();
+
+            Group[i]=new Rucksack(backpackItems);
+            if (i==2)
+            {
+                Group[0].findGroupOverlap(Group[1],Group[2]);
+                totalGroupPriority+=Group[0].getPriority();
+            }
+            i=Math.floorMod(++i, 3);
+
         }
         input.close();
 
-        System.out.println("The total priority is "+totalPriority);
+        System.out.println("The total priority individually is "+totalSinglePriority);
+        System.out.println("The total group priority is "+totalGroupPriority);
     }
-    
 }
--- Day 3/Rucksack.java
+++ Day 3/Rucksack.java
@@ -3,32 +3,35 @@ import java.util.stream.Collectors;
 
 public class Rucksack {
 
+    private Set<Character> fullRucksack=new TreeSet<>();
     private Set<Character> compartment1=new TreeSet<>();
     private Set<Character> compartment2=new TreeSet<>();
+    private Set<Character> overlap;
     private int priority;
 
     public Rucksack (String items)
     {
         priority=0;
         int length=items.length();
+
         String compartment1String=items.substring(0, length/2);
         String compartment2String=items.substring(length/2,length);
+
+        fullRucksack=items.chars().mapToObj(chr -> (char) chr).collect(Collectors.toSet());
         compartment1=compartment1String.chars().mapToObj(chr -> (char) chr).collect(Collectors.toSet());
         compartment2=compartment2String.chars().mapToObj(chr -> (char) chr).collect(Collectors.toSet());
-
-        findOverlap();
-        calculatePriority();
     }
 
-    private void findOverlap()
+    public void findSingleBackpackOverlap()
     {
-        compartment1.retainAll(compartment2);
+        overlap=compartment1;
+        overlap.retainAll(compartment2);
     }
 
     private void calculatePriority()
     {
         int thisPriority=0;
-        for (Character character : compartment1) {
+        for (Character character : overlap) {
             thisPriority=character-38;
             if (Character.isLowerCase(character))
             {
@@ -40,7 +43,15 @@ public class Rucksack {
 
     public int getPriority()
     {
+        calculatePriority();
         return priority;
+    }
+
+    public void findGroupOverlap(Rucksack member2, Rucksack member3)
+    {
+        overlap=fullRucksack;
+        overlap.retainAll(member2.fullRucksack);
+        overlap.retainAll(member3.fullRucksack);
     }
     
 }
