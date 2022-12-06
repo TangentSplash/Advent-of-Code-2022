da615e9f87f349352deff7c6af5befe9a84dc171
TangentSplash <68692742+TangentSplash@users.noreply.github.com>
2022-12-06 12:22:23 +0000

    Day 6 Part 2
    Day 6 Part 2 Completed

--- Day 6/PacketHeader.java
+++ Day 6/PacketHeader.java
@@ -1,5 +1,5 @@
 /* Advent of Code 2022
- * Day 6 Part 1
+ * Day 6 Part 2
  */
 
 import java.io.BufferedReader;
@@ -7,34 +7,50 @@ import java.io.FileReader;
 import java.util.*;
 
 public class PacketHeader {
-    private final static int MARKER_LENGTH=4;
+    private final static int START_OF_PACKET_MARKER_LENGTH=4;
+    private final static int START_OF_MESSAGE_MARKER_LENGTH=14;
+
     public static void main(String[] args) throws Exception
     {
 
         FileReader file = new FileReader("Day 6\\input.txt");
         BufferedReader input = new BufferedReader(file);
 
-        LimitedQueue<Character> currentlyChecking=new LimitedQueue<Character>(MARKER_LENGTH);
+        LimitedQueue<Character> checkingPacketStart=new LimitedQueue<Character>(START_OF_PACKET_MARKER_LENGTH);
+        LimitedQueue<Character> checkingMessageStart=new LimitedQueue<Character>(START_OF_MESSAGE_MARKER_LENGTH);
 
         int in;
         char character;
-        int marker=1;
+        int packetMarker=0;
+        int messageMarker=1;
+        boolean lookingForPacket=true;
         while((in=input.read())!=-1)
         {
             character=(char)in;
-            currentlyChecking.add(character);
-            Set<Character> uniqueCharacters=new TreeSet<Character>(currentlyChecking);
-            if (uniqueCharacters.size()==MARKER_LENGTH)
+            checkingMessageStart.add(character);
+            
+            if(lookingForPacket)
+            {
+                checkingPacketStart.add(character);
+                Set<Character> uniquePacketCharacters=new TreeSet<Character>(checkingPacketStart);
+                if (uniquePacketCharacters.size()==START_OF_PACKET_MARKER_LENGTH)
+                {
+                    lookingForPacket=false;
+                }
+                packetMarker++;
+            }
+
+            Set<Character> uniqueMessageCharacters=new TreeSet<Character>(checkingMessageStart);
+            if (uniqueMessageCharacters.size()==START_OF_MESSAGE_MARKER_LENGTH)
             {
                 break;
             }
-            marker++;
-
+            messageMarker++;
         }
         input.close();
 
-        System.out.println("The first start-of-packet marker is detected after " + marker + " characters");
-
+        System.out.println("The first start-of-packet marker is detected after " + packetMarker + " characters");
+        System.out.println("The first start-of-message marker is detected after " + messageMarker + " characters");
     }
     
 }
