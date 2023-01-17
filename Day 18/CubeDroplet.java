53b8f452e5aa0dc09af9715442e9504282ceb393
TangentSplash <68692742+TangentSplash@users.noreply.github.com>
2023-01-17 16:53:58 +0000

    Day 18 Part 2
    Day 18 Part 2 first attempt thoughts

--- Day 18/CubeDroplet.java
+++ Day 18/CubeDroplet.java
@@ -5,6 +5,10 @@ public class CubeDroplet
     private int z;
 
     private int surfaceArea;
+    private int externalSurfaceArea;
+
+    private boolean[] exposedFaces;
+
 
     public CubeDroplet(int x,int y,int z)
     {
@@ -13,32 +17,36 @@ public class CubeDroplet
         this.z=z;
 
         this.surfaceArea=6*LavaDroplets.SIDE_LENGTH*LavaDroplets.SIDE_LENGTH;
+        this.externalSurfaceArea=surfaceArea;
+        this.exposedFaces=new boolean[6];
     }
 
     public void searchFaces(CubeDroplet[][][] allDroplets)
     {
-        searchLocation(x+1, y, z, allDroplets);
-        searchLocation(x-1, y, z, allDroplets);
-        searchLocation(x, y+1, z, allDroplets);
-        searchLocation(x, y-1, z, allDroplets);
-        searchLocation(x, y, z+1, allDroplets);
-        searchLocation(x, y, z-1, allDroplets);
+        exposedFaces[0]=searchLocation(x+1, y, z, allDroplets);
+        exposedFaces[1]=searchLocation(x-1, y, z, allDroplets);
+        exposedFaces[2]=searchLocation(x, y+1, z, allDroplets);
+        exposedFaces[3]=searchLocation(x, y-1, z, allDroplets);
+        exposedFaces[4]=searchLocation(x, y, z+1, allDroplets);
+        exposedFaces[5]=searchLocation(x, y, z-1, allDroplets);
     }
 
-    private void searchLocation(int x,int y,int z,CubeDroplet[][][] allDroplets)
+    private boolean searchLocation(int x,int y,int z,CubeDroplet[][][] allDroplets)
     {
         if(x>=0 && x<allDroplets[0][0].length && y>=0 && y<allDroplets[0].length && z>=0 && z<allDroplets.length)
         {
             if(allDroplets[z][y][x]!=null)
             {
                 surfaceArea--;
+                return true;
             }
         }
+        return false;
     }
 
-    public int getExposedSurfaceArea()
+    public int getAllExposedSurfaceArea()
     {
+        externalSurfaceArea=surfaceArea;
         return surfaceArea;
     }
-    
 }
--- Day 18/LavaDroplets.java
+++ Day 18/LavaDroplets.java
@@ -8,18 +8,24 @@ import java.util.*;
 public class LavaDroplets 
 {
     public static final int SIDE_LENGTH=1;
+    private final int SEARCH_X=0;
+    private final int SEARCH_Y=1;
+    private final int SEARCH_Z=2;
+
     private int xMax;
     private int yMax;
     private int zMax;
     
     CubeDroplet[][][] dropletsInSpace;
     List<CubeDroplet> dropletList;
+    Set<CubeDroplet> externalDroplets;
 
     public LavaDroplets() throws Exception
     {
-        File inputFile = new File("Day 18/input.txt");
+        File inputFile = new File("Day 18/inputtest.txt");
         Scanner input = new Scanner(inputFile);
         dropletList=new ArrayList<CubeDroplet>();
+        externalDroplets=new HashSet<CubeDroplet>();
 
         interpretInput(input);
         input.close();
@@ -38,12 +44,26 @@ public class LavaDroplets
                 }
             }
         }
-        int exposedSurfaceArea=0;
+        int allExposedSurfaceArea=0;
         for (CubeDroplet cubeDroplet : dropletList) 
         {
-            exposedSurfaceArea+=cubeDroplet.getExposedSurfaceArea();
+            allExposedSurfaceArea+=cubeDroplet.getAllExposedSurfaceArea();
         }
-        System.out.println("The estimated surface area is "+exposedSurfaceArea);
+        System.out.println("The estimated total surface area is "+allExposedSurfaceArea);
+
+        scanDirection(yMax, zMax, xMax, SEARCH_X,false);
+        scanDirection(yMax, zMax, xMax, SEARCH_X,true);
+        scanDirection(xMax, zMax, yMax, SEARCH_Y,false);
+        scanDirection(xMax, zMax, yMax, SEARCH_Y,true);
+        scanDirection(xMax, yMax, zMax, SEARCH_Z,false);
+        scanDirection(xMax, yMax, zMax, SEARCH_Z,true);
+
+        int externalSurfaceArea=0;
+        for (CubeDroplet cubeDroplet : externalDroplets) 
+        {
+            externalSurfaceArea+=cubeDroplet.getAllExposedSurfaceArea();
+        }
+        System.out.println("The estimated total external surface area is "+externalSurfaceArea);
     }
 
     private void interpretInput(Scanner input)
@@ -83,5 +103,106 @@ public class LavaDroplets
             dropletsInSpace[z][y][x]=newDroplet;
             dropletList.add(newDroplet);
         }
+    }
+
+    /***
+     * @param aMax
+     * @param bMax
+     * @param cMax Dimension that is being searched along
+     * @param searchDimension 0: search along x, a is y,b is z. 1: along y, a is x, b is z. 2: along z, a is x, b is y.  
+     * @param searchBack search from max down to zero in search dimension
+     */
+    private void scanDirection(int aMax,int bMax,int cMax,int searchDimension,boolean searchBack)
+    {
+        for (int a=0;a<aMax;a++)
+        {
+            for(int b=0;b<bMax;b++)
+            {
+                for (int c=0;c<cMax;c++)
+                {
+                    int i=0,j=0,k=0,searchValue=c;
+                    if(searchBack)
+                    {
+                        searchValue=cMax-c;
+                    }
+                    switch (searchDimension)
+                    {
+                        case 0:
+                        {
+                            i=searchValue;
+                            j=a;
+                            k=b;
+                            break;
+                        }
+                        case 1:
+                        {
+                            j=searchValue;
+                            i=a;
+                            k=b;
+                            break;
+                        }
+                        case 2:
+                        {
+                            k=searchValue;
+                            i=a;
+                            j=b;
+                            break;
+                        }
+                    }
+                    CubeDroplet currentDroplet=dropletsInSpace[k][j][i];
+                    if(currentDroplet!=null)
+                    {
+                        externalDroplets.add(currentDroplet);
+                        if(searchFurther(c,cMax,a,b,searchDimension,searchBack))    //If there is another droplet along this direction, it means there is  
+                        {
+
+                        }
+                        break;  //Stop searching in this dimension, search a different space
+                    }
+                }
+            }
+        }
+    }
+
+    private boolean searchFurther(int start,int end,int a,int b,int searchDimension,boolean searchBack)
+    {
+        for (int d=start;d<end;d++)
+        {
+            int i=0,j=0,k=0,searchValue=d;
+            if(searchBack)
+            {
+                searchValue=end-d;
+            }
+            switch (searchDimension)
+            {
+                case 0:
+                {
+                    i=searchValue;
+                    j=a;
+                    k=b;
+                    break;
+                }
+                case 1:
+                {
+                    j=searchValue;
+                    i=a;
+                    k=b;
+                    break;
+                }
+                case 2:
+                {
+                    k=searchValue;
+                    i=a;
+                    j=b;
+                    break;
+                }
+            }
+            CubeDroplet currentDroplet=dropletsInSpace[k][j][i];
+            if(currentDroplet!=null)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
